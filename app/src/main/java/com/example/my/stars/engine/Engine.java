29f67930b401a1d7394c0ebe459fb63293f264f5
Tolik <t1005828@gmail.com>
2023-12-29 11:02:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/my/stars/engine/Engine.java
@@ -0,0 +1,79 @@
+package com.example.my.stars.engine;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class Engine {
+
+    private final Model model;
+    private final Render render;
+
+    private final SurfaceHolder.Callback callback;
+    private SurfaceHolder surfaceHolder;
+
+    private volatile boolean stopped;
+
+    long time = System.nanoTime();
+
+    Runnable threadRunnable = new Runnable() {
+        @Override
+        public void run() {
+            while (!stopped) {
+                Canvas canvas;
+                if (surfaceHolder == null || (canvas = surfaceHolder.lockCanvas()) == null) {
+                    synchronized (Engine.this) {
+                        try {
+                            Engine.this.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    long timeElapsed = System.nanoTime() - time;
+                    time = System.nanoTime();
+                    model.update(timeElapsed);
+                    render.draw(canvas, model);
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+            }
+        }
+    };
+
+    public void stop() {
+        this.stopped = true;
+    }
+
+    public Engine(SurfaceView surfaceView) {
+        model = new Model();
+        render = new Render();
+
+        Thread engineThread = new Thread(threadRunnable, "EngineThread");
+        engineThread.start();
+
+        callback = new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+                Engine.this.surfaceHolder = surfaceHolder;
+                synchronized (Engine.this) {
+                    Engine.this.notifyAll();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+                Engine.this.surfaceHolder = surfaceHolder;
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+                Engine.this.surfaceHolder = null;
+            }
+        };
+
+        surfaceView.getHolder().addCallback(callback);
+    }
+
+}
---
+++ app/src/main/java/com/example/my/stars/engine/Point.java
@@ -0,0 +1,16 @@
+package com.example.my.stars.engine;
+
+public class Point {
+    float x;
+    float y;
+    float z;
+    int color;
+
+    public Point(float x, float y, float z, int color) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.color = color;
+    }
+
+}
